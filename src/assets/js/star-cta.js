05bc5f528ab1558862a46eda0eddd30c0f0f8fc2
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2021-01-07 15:47:48 -0800

    Only show once per user. Also show after scroll.

--- src/assets/js/star-cta.js
+++ src/assets/js/star-cta.js
@@ -1,19 +1,40 @@
 
-var mouseY = 0;
 
 $(document).ready(function() {
+    var mouseY = 0;
+    var scrolled = false;
 
-    document.addEventListener("mousemove", function(e) {
+    $(document).mousemove(function(e) {
         mouseY = e.clientY;
     });
 
     $(document).mouseleave(function () {
         if (mouseY < 100) {
-            $('.star-cta').attr({
-                "data-balloon-visible": "",
-                "aria-label": "Bookmark me for later!",
-                "data-balloon-pos": "down",
-            });
+            triggerStarCta();
+        }
+    });
+
+    $(document).scroll(function() {
+        if (!scrolled) {
+            setTimeout(function() {
+                triggerStarCta();
+            }, 5000);
+            scrolled = true;
         }
     });
 });
+
+function triggerStarCta() {
+    if (localStorage && localStorage.getItem('earthly-star-cta-shown') === 'true') {
+        return;
+    }
+
+    $('.star-cta').attr({
+        "data-balloon-visible": "",
+        "aria-label": "Bookmark me for later!",
+        "data-balloon-pos": "down",
+    });
+    if (localStorage) {
+        localStorage.setItem('earthly-star-cta-shown', 'true')
+    }
+}
