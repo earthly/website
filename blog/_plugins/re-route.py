2e34c5f139f5d4acd300ebaa3d472d4924754c92
Adam Bell <adam@earthly.dev>
2021-04-22 09:05:45 -0400

    Article: Green Programming Languages Vs Brown Programming Languages (#36)
    * wip
    
    * draft of article
    
    * wip
    
    * wip
    
    * adding header image
    
    * adding quote
    
    * Adding blog revisions
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Vlad A. Ionescu <446771+vladaionescu@users.noreply.github.com>
    
    * wip
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * Update blog/_posts/2021-01-01-brown-green-language.md
    
    Co-authored-by: Corey Larson <corey@eatrunco.de>
    
    * adding header and trying to fix graphs
    
    * fixing brown / green images
    
    Co-authored-by: Vlad A. Ionescu <446771+vladaionescu@users.noreply.github.com>
    Co-authored-by: Corey Larson <corey@eatrunco.de>

--- blog/_plugins/re-route.py
+++ blog/_plugins/re-route.py
@@ -3,14 +3,14 @@
 from pandocfilters import toJSONFilter, Image
 
 path_from = "_site/assets/images/"
-path_to = "blog/assets/images/"
+path_to = "/blog/assets/images/"
 
 def reroute(key, value, format, meta):
   if key == 'Image':
     attrs, alttext, (url, title) = value
     if url.startswith(path_from):
         newurl = path_to + url.split(path_from)[-1]
-    return Image(attrs, alttext, (newurl, title))
+        return Image(attrs, alttext, (newurl, title))
 
 if __name__ == "__main__":
   toJSONFilter(reroute)
