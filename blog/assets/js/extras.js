9a689d81d9b6faeb862bfa4ff3198c28deaa17de
Adam Gordon Bell <adam@corecursive.com>
2021-10-21 10:02:26 -0400

    clean up PR

--- blog/assets/js/extras.js
+++ blog/assets/js/extras.js
@@ -16,17 +16,12 @@ function syntaxLabel() {
       var caption = b.getAttribute('data-caption');
       var elem;
       if(b.tagName == "PRE"){
-        console.log("pre")
         elem = b;
       } else if (b.tagName == "DIV" && b.firstChild.tagName == "PRE") {
-        console.log("nested pre")
         elem = b.firstChild;
       }
-      console.log(elem)
       if(elem)
       {
-        console.log("setting")
-        // elem.innerHTML = "<span class=\"syntax-label\">"+caption+"<img src=\"/blog/assets/images/iTerm-icon.png\"/ width=\"24\" height=\"24\"></span>" + elem.innerHTML;
         elem.innerHTML = "<span class=\"syntax-label\">"+caption+"</span>" + elem.innerHTML;
       }
     }
