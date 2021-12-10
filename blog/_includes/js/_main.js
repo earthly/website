d3a3ca5d1eccb7f1e782ac00cbeca76dba91f9d6
Adam Gordon Bell <adam@corecursive.com>
2021-12-10 16:34:26 -0500

    fixing jquery error

--- blog/_includes/js/_main.js
+++ blog/_includes/js/_main.js
@@ -21,12 +21,6 @@ $(document).ready(function() {
     }
   };
 
-  // stickySideBar();
-
-  // $(window).resize(function() {
-  //   stickySideBar();
-  // });
-
   // Follow menu drop down
   $(".author__urls-wrapper button").on("click", function() {
     $(".author__urls").toggleClass("is--visible");
@@ -57,41 +51,6 @@ $(document).ready(function() {
   $(
     "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']"
   ).addClass("image-popup");
-
-  // Magnific-Popup options
-  // $(".image-popup").magnificPopup({
-  //   // disableOn: function() {
-  //   //   if( $(window).width() < 500 ) {
-  //   //     return false;
-  //   //   }
-  //   //   return true;
-  //   // },
-  //   type: "image",
-  //   tLoading: "Loading image #%curr%...",
-  //   gallery: {
-  //     enabled: true,
-  //     navigateByImgClick: true,
-  //     preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-  //   },
-  //   image: {
-  //     tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
-  //   },
-  //   removalDelay: 500, // Delay in milliseconds before popup is removed
-  //   // Class that is added to body when popup is open.
-  //   // make it unique to apply your CSS animations just to this exact popup
-  //   mainClass: "mfp-zoom-in",
-  //   callbacks: {
-  //     beforeOpen: function() {
-  //       // just a hack that adds mfp-anim class to markup
-  //       this.st.image.markup = this.st.image.markup.replace(
-  //         "mfp-figure",
-  //         "mfp-figure mfp-with-anim"
-  //       );
-  //     }
-  //   },
-  //   closeOnContentClick: true,
-  //   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-  // });
 
   // Add anchors for headings
   $('.page__content').find('h2, h3, h4, h5, h6').each(function() {
