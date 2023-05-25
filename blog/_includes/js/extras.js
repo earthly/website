26f6a5eec7c251505d7086410c4097c2071012cc
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-05-25 10:57:33 +0530

    Copy button in code blocks

--- blog/_includes/js/extras.js
+++ blog/_includes/js/extras.js
@@ -8,57 +8,16 @@ function externalLinks() {
 
 externalLinks();
 
-
-// Add label to code blocks
-//.caption
-function addStuff(label, action){
-  for(var c = document.querySelectorAll('['+ label +']') , a = 0;a < c.length; a++) {
-    var b = c[a];
-    var caption = b.getAttribute(label);
-    var elem;
-    if(b.tagName == "PRE"){
-      elem = b;
-    } else if (b.tagName == "DIV" && b.firstChild.tagName == "PRE") {
-      elem = b.firstChild;
-    }
-    if(elem)
-    {
-      action(elem, caption)
-    }
-  }
-}
+// Customize code blocks
 function syntaxLabel() {
-    //mobile version
-    // use ~~~{captionm="filename"}
-    addStuff("data-caption",
-    function (elem, caption) {
-      elem.innerHTML =  "<span class=\"syntax-label\">"+caption+"</span>" + elem.innerHTML ; 
-    }
-    );
-
-    //wide caption : too wide to show on mobile
-    // use ~~~{captionw="filename"}
-    addStuff("data-captionw",
-     function (elem, caption) {
-      elem.innerHTML =  "<span class=\"syntax-label-wide\">"+caption+"</span>" + elem.innerHTML ; 
-     }
-    );
-
-    // bottom caption
-    // use ~~~{captionb="filename"}
-    addStuff("data-captionb",
-      function (elem, caption) {
-         elem.innerHTML = elem.innerHTML + "<span class=\"syntax-label-bottom\">"+caption+"</span>" ; 
-       }
-    );
-
-    // bottom caption
-    // use ~~~{bcaptionb="filename"}
-    addStuff("data-bcaption",
-    function (elem, caption) {
-       elem.innerHTML = elem.innerHTML + "<span class=\"syntax-label-bottom\">"+caption+"</span>" ; 
-     }
-  );
+  for (var c = document.querySelectorAll("pre.sourceCode"), a = 0; a < c.length; a++) {
+    var b = c[a];
+    var caption = b.parentElement.getAttribute('data-caption');
+    b.innerHTML =
+      `<div class="toolbar"><div>${caption || "Terminal"}</div><div class="copy-item" data-clipboard-text="${
+        b.querySelector("code").innerText
+      }">Copy</div></div>` + b.innerHTML;
+  };
 };
 
 syntaxLabel();
