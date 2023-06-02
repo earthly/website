0a3a25d403517f428393e347f74479f2f7d2af79
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-06-02 13:12:51 +0530

    Removed header for >_ codes

--- blog/_includes/js/extras.js
+++ blog/_includes/js/extras.js
@@ -13,19 +13,12 @@ function syntaxLabel() {
   for (var c = document.querySelectorAll("pre.sourceCode:not(.merge-code)"), a = 0; a < c.length; a++) {
     var b = c[a];
     var caption = b.parentElement.getAttribute("data-caption");
-    if (caption) {
+    if (caption && caption != ">_") {
       b.classList.add("pt-11");
-
-      if (caption == ">_") {
-        b.innerHTML =
-          `<div class="toolbar"><div class="terminal">${caption}</div><div>Terminal</div></div>` +
-          b.innerHTML;
-      } else {
-        b.innerHTML =
-          `<div class="toolbar"><div>${caption}</div><div class="copy-item" data-clipboard-text="${
-            b.querySelector("code").innerText.replace(/"/g, '&quot;')
-          }">Copy</div></div>` + b.innerHTML;
-      };
+      b.innerHTML =
+        `<div class="toolbar"><div>${caption}</div><div class="copy-item" data-clipboard-text="${
+          b.querySelector("code").innerText.replace(/"/g, '&quot;')
+        }">Copy</div></div>` + b.innerHTML;
     };
   };
 };
