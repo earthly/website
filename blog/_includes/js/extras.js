f80515071a7947ea77e3760af9abd359465c8521
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-06-06 12:15:31 +0530

    Fixed outputs

--- blog/_includes/js/extras.js
+++ blog/_includes/js/extras.js
@@ -8,19 +8,64 @@ function externalLinks() {
 
 externalLinks();
 
-// Customize code blocks
-function syntaxLabel() {
-  for (var c = document.querySelectorAll("pre.sourceCode:not(.merge-code)"), a = 0; a < c.length; a++) {
+// Add label to code blocks
+//.caption
+function addStuff(label, action){
+  for(var c = document.querySelectorAll('['+ label +']') , a = 0;a < c.length; a++) {
     var b = c[a];
-    var caption = b.parentElement.getAttribute("data-caption");
-    if (caption && caption != ">_") {
-      b.classList.add("pt-11");
-      b.innerHTML =
-        `<div class="toolbar"><div>${caption}</div><div class="copy-item" data-clipboard-text="${
-          b.querySelector("code").innerText.replace(/"/g, '&quot;')
-        }">Copy</div></div>` + b.innerHTML;
-    };
-  };
+    var caption = b.getAttribute(label);
+    var elem;
+    if(b.tagName == "PRE"){
+      elem = b;
+    } else if (b.tagName == "DIV" && b.firstChild.tagName == "PRE") {
+      elem = b.firstChild;
+    }
+    if(elem && caption) {
+      if (!elem.classList.contains('output') && !["Results", "Output", ">_"].includes(caption)) {
+        elem.classList.add("relative", "pt-11");
+        elem.innerHTML =
+          `<div class="toolbar"><div>${caption}</div><div class="copy-item" data-clipboard-text="${
+            b.querySelector("code").innerText.replace(/"/g, '&quot;')
+          }">Copy</div></div>` + elem.innerHTML;
+      } else {
+        action(elem, caption)
+      }
+    }
+  }
+}
+
+function syntaxLabel() {
+    //mobile version
+    // use ~~~{captionm="filename"}
+    addStuff("data-caption",
+    function (elem, caption) {
+      elem.innerHTML =  "<span class=\"syntax-label\">"+caption+"</span>" + elem.innerHTML ; 
+    }
+    );
+
+    //wide caption : too wide to show on mobile
+    // use ~~~{captionw="filename"}
+    addStuff("data-captionw",
+     function (elem, caption) {
+      elem.innerHTML =  "<span class=\"syntax-label-wide\">"+caption+"</span>" + elem.innerHTML ; 
+     }
+    );
+
+    // bottom caption
+    // use ~~~{captionb="filename"}
+    addStuff("data-captionb",
+      function (elem, caption) {
+         elem.innerHTML = elem.innerHTML + "<span class=\"syntax-label-bottom\">"+caption+"</span>" ; 
+       }
+    );
+
+    // bottom caption
+    // use ~~~{bcaptionb="filename"}
+    addStuff("data-bcaption",
+    function (elem, caption) {
+       elem.innerHTML = elem.innerHTML + "<span class=\"syntax-label-bottom\">"+caption+"</span>" ; 
+     }
+  );
 };
 
 syntaxLabel();
