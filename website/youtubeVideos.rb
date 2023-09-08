74de4c55d2fd69e877c4fb18df858fa80a94d61a
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-09-08 19:01:01 +0530

    Added pagination

--- website/youtubeVideos.rb
+++ website/youtubeVideos.rb
@@ -4,20 +4,38 @@ require 'uri'
 
 puts "Fetching youtube videos for webinars page"
 
-begin
-  uri = URI("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA")
+def fetchVideos (videos, error, nextPageToken)
+  uri = URI("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA#{ "&pageToken=#{nextPageToken}" if nextPageToken }")
   res = Net::HTTP.get_response(uri)
 
   if res.code == "200"
-    videos = []
-    JSON.parse(res.body)['items'].each do |video|
+    body = JSON.parse(res.body)
+
+    body['items'].each do |video|
       videos << { title: video['snippet']['title'], videoId: video['snippet']['resourceId']['videoId'], type: 'Video' }
     end
+
+    return body['nextPageToken']
   end
 
-  File.write("_data/webinar-videos.json", videos.to_json)
+  error = true
+  puts "Error fetching youtube data: #{res.body}"
 
-  puts "Updated videos data in _data directory" if res.code == "200"
-  puts "Error fetching youtube data: #{res.body}" if res.code != "200"
+  return nil
+end
+
+begin
+  videos = []
+  error = false
+
+  response = fetchVideos(videos, error, nil)
+  while response
+    response = fetchVideos(videos, error, response)
+  end
+
+  if !error
+    puts "Updated videos data in _data directory"
+    File.write("_data/webinar-videos.json", videos.to_json)
+  end
 rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
 end
