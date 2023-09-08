bec8398f6f1392fd18133a370bd4d3536fb83e11
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-09-08 13:40:47 +0530

    Handled exceptions

--- website/youtubeVideos.rb
+++ website/youtubeVideos.rb
@@ -5,16 +5,19 @@ require 'uri'
 puts "Fetching youtube videos"
 
 begin
-  uri = URI('https://youtue.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA')
+  uri = URI('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA')
   res = Net::HTTP.get_response(uri)
 
-  videos = []
-  JSON.parse(res.body)['items'].each do |video|
-    videos << { title: video['snippet']['title'], videoId: video['snippet']['resourceId']['videoId'], type: 'Video' }
+  if res.code == "200"
+    videos = []
+    JSON.parse(res.body)['items'].each do |video|
+      videos << { title: video['snippet']['title'], videoId: video['snippet']['resourceId']['videoId'], type: 'Video' }
+    end
   end
 
   File.write("_data/webinar-videos.json", videos.to_json)
-  puts "Updated videos data"
-rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-  Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+
+  puts "Updated videos data" if res.code == "200"
+  puts "Error fetching youtube data: #{res.body}" if res.code != "200"
+rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
 end
