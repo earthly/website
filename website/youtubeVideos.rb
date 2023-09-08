f4da99cf070919a5d19f49cfd24d006e8ea4201f
avinder-red-crackle <avinderpal.singh@redcrackle.com>
2023-09-08 14:23:54 +0530

    Default data added

--- website/youtubeVideos.rb
+++ website/youtubeVideos.rb
@@ -2,10 +2,10 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-puts "Fetching youtube videos"
+puts "Fetching youtube videos for webinars page"
 
 begin
-  uri = URI('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA')
+  uri = URI("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUlROK3yIuIyJWmt-gnkAjlQ&key=AIzaSyCKS2_U1c9NGRlcobOjN9MEpV1WGGWVHSA")
   res = Net::HTTP.get_response(uri)
 
   if res.code == "200"
@@ -17,7 +17,7 @@ begin
 
   File.write("_data/webinar-videos.json", videos.to_json)
 
-  puts "Updated videos data" if res.code == "200"
+  puts "Updated videos data in _data directory" if res.code == "200"
   puts "Error fetching youtube data: #{res.body}" if res.code != "200"
 rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
 end
