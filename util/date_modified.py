190dd5659bcc375bb6a3599a3c0009d1f102b3d8
Adam Gordon Bell <adam@corecursive.com>
2023-10-27 10:25:02 -0400

    WIP

--- util/date_modified.py
+++ util/date_modified.py
@@ -10,21 +10,28 @@ for filename in os.listdir(POSTS_DIR):
     if filename.endswith(".md"):
         filepath = os.path.join(POSTS_DIR, filename)
         
-        # Get last modified date from Git
-        date = subprocess.check_output(["git", "log", "-1", "--format=%ad", "--", filepath]).decode('utf-8').strip()
+        # Get last modified date from Git in the format YYYY-MM-DD
+        date = subprocess.check_output(["git", "log", "-1", "--format=%ad", "--date=short", "--", filepath]).decode('utf-8').strip()
         
         # Read the file
         with open(filepath, 'r') as file:
-            content = file.read().split("---")
-            front_matter = yaml.safe_load(content[1])
+            content = file.readlines()
+            delimiter_count = content.count('---\n')
             
-            # Update or add the 'updated' field
-            front_matter['updated'] = date
+            if delimiter_count < 2:
+                continue  # Skip files that don't have a proper front matter
+
+            front_matter_index = content.index('---\n') + 1  # index after the first '---'
+            end_front_matter_index = front_matter_index + content[front_matter_index:].index('---\n')  # index of the second '---'
+
+            front_matter = yaml.safe_load("".join(content[front_matter_index:end_front_matter_index]))
             
-            # Combine the content
-            content[1] = yaml.dump(front_matter)
-            updated_content = "---".join(content)
+            # Update or add the 'last_modified_at' field
+            front_matter['last_modified_at'] = date
+            
+            # Replace the old front matter with the updated one
+            content[front_matter_index:end_front_matter_index] = yaml.dump(front_matter, default_flow_style=False, allow_unicode=True).splitlines()
             
         # Write the updated content back to the file
         with open(filepath, 'w') as file:
-            file.write(updated_content)
+            file.write("\n".join(content))
