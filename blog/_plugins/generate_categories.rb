91b1ad8ac1faeb31de53ab5afae7ec6e6d7e8af7
Adam Gordon Bell <adam@corecursive.com>
2024-05-27 10:27:00 -0400

    WIP

---
+++ blog/_plugins/generate_categories.rb
@@ -0,0 +1,30 @@
+module Jekyll
+    class CategoryPageGenerator < Generator
+      safe true
+  
+      def generate(site)
+        if site.layouts.key? 'category'
+          site.data['categories'].each do |category|
+            site.pages << CategoryPage.new(site, site.source, category['slug'], category['name'], category['title'])
+          end
+        end
+      end
+    end
+  
+    class CategoryPage < Page
+      def initialize(site, base, slug, name, title)
+        @site = site
+        @base = base
+        @dir  = "categories/#{slug}"
+        @name = 'index.html'
+  
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), 'category.html')
+        self.data['title'] = title
+        self.data['slug'] = slug
+        self.data['permalink'] = "/categories/#{slug}/"
+        self.data['category_name'] = name
+      end
+    end
+  end
+  
